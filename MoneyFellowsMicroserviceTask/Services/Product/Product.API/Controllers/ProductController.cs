a567e4ecd86567f1cda60fee8a8e310d078d297b
passantIhab <passant.ihab@outlook.com>
2024-05-02 19:10:19 +0300

    API Layer init

---
+++ MoneyFellowsMicroserviceTask/Services/Product/Product.API/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Products.Application.Features.Queries.GetProductsList;
+using System.Net;
+
+namespace Products.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+
+        [HttpGet("{brandName}", Name = "GetProduct")]
+        [ProducesResponseType(typeof(IEnumerable<ProductsDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<ProductsDTO>>> GetProductsByBrandName(string brandName)
+        {
+            var query = new GetProductsListByBrandNameQuery(brandName);
+            var products = await _mediator.Send(query);
+            return Ok(products);
+        }
+    }
+}
---
+++ MoneyFellowsMicroserviceTask/Services/Product/Product.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Products.Application.Behaviours;
+using Products.Application.Mappings;
+using System.Reflection;
+
+namespace Products.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof(MappingProfile));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnHandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
