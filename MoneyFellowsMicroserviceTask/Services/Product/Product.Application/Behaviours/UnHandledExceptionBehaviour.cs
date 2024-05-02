f680c8a71a9d9be61ffa262c57f6b3285dbae2bb
passantIhab <passant.ihab@outlook.com>
2024-05-02 18:12:52 +0300

    Add Validation/Exception behaviours

---
+++ MoneyFellowsMicroserviceTask/Services/Product/Product.Application/Behaviours/UnHandledExceptionBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Application.Behaviours
+{
+    public class UnHandledExceptionBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                // log the exception
+                throw;
+            }
+        }
+    }
+}
